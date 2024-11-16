baf07d8a3ac7ce4069621509bbbd4ba1066db70f
Thiago Santos <tos.oliveira@gmail.com>
2024-11-16 04:53:33 -0300

    fix: refsactoring

--- src/Codibre.EnumerableExtensions.Branching/Internal/LinkedNode.cs
+++ src/Codibre.EnumerableExtensions.Branching/Internal/LinkedNode.cs
@@ -3,7 +3,7 @@
 internal sealed record LinkedNode<T>
 {
     public T Value { get; }
-    public Lazy<ValueTask<LinkedNode<T>?>>? Next { get; private set; }
+    public Lazy<ValueTask<LinkedNode<T>?>> Next { get; private set; }
 
     public LinkedNode(T value, IBranchContext<T> context)
     {
@@ -11,7 +11,9 @@ internal sealed record LinkedNode<T>
         Next = new(context.FillNext, LazyThreadSafetyMode.ExecutionAndPublication);
     }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     private LinkedNode(T value) => Value = value;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
     public static LinkedNode<T>? New(IEnumerator<T> source, BranchRunOptions options, IBranchContext<T> context)
     {
@@ -24,17 +26,45 @@ internal sealed record LinkedNode<T>
         return root;
     }
 
-    public static LinkedNode<T> Root(IEnumerator<T> source, BranchRunOptions options, IBranchContext<T> context)
+    public static async ValueTask<LinkedNode<T>?> New(IAsyncEnumerator<T> source, BranchRunOptions options, IBranchContext<T> context)
+    {
+        if (!await source.MoveNextAsync()) return null;
+        var preload = options.Limit;
+        LinkedNode<T> root = new(source.Current);
+        var node = root;
+        while (preload-- > 0 && await source.MoveNextAsync()) node = (node.Next = new(ValueTask.FromResult<LinkedNode<T>?>(new(source.Current)))).Value.Result!;
+        node.Next = new(context.FillNext, LazyThreadSafetyMode.ExecutionAndPublication);
+        return root;
+    }
+
+    public static LinkedNode<T> Root(IEnumerator<T> source, BranchRunOptions options)
         => new(default(T)!)
         {
-            Next = new(ValueTask.FromResult(New(source, options, context)))
+            Next = new(
+                    ValueTask.FromResult(
+                        New(
+                            source,
+                            options,
+                            new AsyncBranchContext<T>(
+                                (c) => ValueTask.FromResult(LinkedNode<T>.New(source, options, c))
+                            )
+                        )
+                    )
+                )
         };
 
-    public static LinkedNode<T> Root(Func<IBranchContext<T>, ValueTask<LinkedNode<T>?>> getNext, BranchRunOptions options)
-    {
-        IBranchContext<T> context = options.Limit <= 1
-            ? new AsyncBranchContext<T>(getNext)
-            : new BranchContext<T>(getNext, options);
-        return new(default!, context);
-    }
+    public static LinkedNode<T> Root(IAsyncEnumerator<T> source, BranchRunOptions options)
+        => new(default(T)!)
+        {
+            Next = new(
+                    New(
+                        source,
+                        BranchRunOptions.Yielder,
+                        new BranchContext<T>(
+                            (c) => LinkedNode<T>.New(source, BranchRunOptions.Yielder, c),
+                            options
+                        )
+                    )
+                )
+        };
 }
